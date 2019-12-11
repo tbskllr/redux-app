1c425d6e885eb50bd21ecd245706c9db48c8b205
Tobias Keller <tbskllr@gmail.com>
2019-12-11 22:48:00 +0100

    comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
 export default class Comments extends Component {
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  }
+
   render() {
-    return <div className="comment">I'm the comment</div>;
+    return (
+      <div className="comment">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" />
+        </form>
+      </div>
+    );
   }
 }
